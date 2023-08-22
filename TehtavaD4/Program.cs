9c52297e5cf731337afcd945314ce2e05fa49581
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-22 05:47:06 +0300

    Add project files.

---
+++ TehtavaD4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ModulusCalculation
+{
+    class Program
+    {
+        static int CalculateModulus(int dividend, int divisor)
+        {
+            // Laske jakojäännös
+            int remainder = dividend % divisor;
+            return remainder;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä jaettava luku: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Syötä jakaja: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            // Laske jakojäännös
+            int remainder = CalculateModulus(dividend, divisor);
+
+            // Tulosta tulos
+            Console.WriteLine($"Jakojäännös: {remainder}");
+        }
+    }
+}
+
